2cdfbf331548df4d9bc081199a7b4509e736b15e
DianaM <37599121+DianaM01@users.noreply.github.com>
2020-04-21 17:47:52 -0700

    pep8 edits

--- tabs/tab_1.py
+++ tabs/tab_1.py
@@ -7,42 +7,42 @@ import os
 from app import app
 import dash_bootstrap_components as dbc
 
-available_scenarios = [name for name in os.listdir('data') if os.path.isdir(os.path.join('data', name)) and name !='data']
+available_scenarios = [name for name in os.listdir('data') if os.path.isdir(os.path.join('data', name)) and name != 'data']
 
 
-tab_1_layout =  dbc.Card(
+tab_1_layout = dbc.Card(
     [
         dbc.CardHeader(html.H1("Select Scenarios")),
         dbc.CardBody(
             [
-        dbc.FormGroup(
-            [
-                dbc.Label("Scenario 1"),
-                dcc.Dropdown(
-                    id="scenario-1-dropdown",
-                    options=[
-                        {"label": col, "value": col} for col in available_scenarios
-                    ],
-                    value=available_scenarios[0],
+                dbc.FormGroup(
+                    [
+                        dbc.Label("Scenario 1"),
+                        dcc.Dropdown(
+                            id="scenario-1-dropdown",
+                            options=[
+                              {"label": col, "value": col} for col in available_scenarios
+                            ],
+                            value=available_scenarios[0],
+                        ),
+                    ]
+                ),
+                dbc.FormGroup(
+                    [
+                        dbc.Label("Scenario 2"),
+                        dcc.Dropdown(
+                            id="scenario-2-dropdown",
+                            options=[
+                                {"label": col, "value": col} for col in available_scenarios
+                            ],
+                            value=available_scenarios[1],
+                        ),
+                    ]
                 ),
             ]
-        ),
-        dbc.FormGroup(
-            [
-                dbc.Label("Scenario 2"),
-                dcc.Dropdown(
-                    id="scenario-2-dropdown",
-                    options=[
-                        {"label": col, "value": col} for col in available_scenarios
-                    ],
-                    value=available_scenarios[1],
-                ),
-            ]
-        ),
-        ]
-            )
+        )
 
     ],
-    #className = 'bg-light',
-    className = 'aside-card'
+    #className='bg-light',
+    className='aside-card'
 )
--- tabs/tab_2.py
+++ tabs/tab_2.py
@@ -10,84 +10,85 @@ from app import app
 import json
 
 
-tab_2_filter =  [dbc.Card(
+tab_2_filter = [dbc.Card(
     [
-    dbc.CardHeader(html.H1('Filters')), 
-    dbc.CardBody(
-        [
-            
-            dbc.Label('Person Type:'),
-            dcc.Dropdown(
-                value='All',
-                id='person-type-dropdown'
-            ),
-            html.Br(),
-            dbc.Label('Destination Purpose:'),
-            dcc.Dropdown(
-                value='All',
-                id='dpurp-dropdown'
+        dbc.CardHeader(html.H1('Filters')),
+        dbc.CardBody(
+            [
+
+                dbc.Label('Person Type:'),
+                dcc.Dropdown(
+                    value='All',
+                    id='person-type-dropdown'
                 ),
-            html.Br(),
-            html.Div(id='df', style={'display': 'none'}),
-            html.Div(id='dummy_div'),
-        ],
-        className = 'bg-light',
-      
-        )],
+                html.Br(),
+                dbc.Label('Destination Purpose:'),
+                dcc.Dropdown(
+                    value='All',
+                    id='dpurp-dropdown'
+                ),
+                html.Br(),
+                html.Div(id='df', style={'display': 'none'}),
+                html.Div(id='dummy_div'),
+            ],
+            className='bg-light',
+
+        )
+    ],
     className='aside-card'
-)  ] 
+)]
 
 tab_2_layout = [
-dbc.Card(
-    dbc.CardBody(
-        [
-            html.H2("Trip Mode Choice"),
-            html.Br(),
-            dbc.RadioItems(
-                id='mode-share-type',
-                options=[{'label': i, 'value': i} for i in ['Mode Share', 'Trips by Mode']],
-                value='Mode Share',
-                inline=True
-            ),
-            dcc.Graph(id='mode-choice-graph'),
-            #html.Br(),
-            #dbc.CardFooter('Trip Departure Hour:'),
-            #dcc.Graph(id='trip-deptm-graph'),
+    dbc.Card(
+        dbc.CardBody(
+            [
+                html.H2("Trip Mode Choice"),
+                html.Br(),
+                dbc.RadioItems(
+                    id='mode-share-type',
+                    options=[{'label': i, 'value': i} for i in ['Mode Share', 'Trips by Mode']],
+                    value='Mode Share',
+                    inline=True
+                ),
+                dcc.Graph(id='mode-choice-graph'),
+                #html.Br(),
+                #dbc.CardFooter('Trip Departure Hour:'),
+                #dcc.Graph(id='trip-deptm-graph'),
 
-            html.Div(id='dummy_div'),
-        ],
+                html.Div(id='dummy_div'),
+            ],
+
+        ),
+        #className='card-deck py-4',
+        style={"margin-top": "20px"},
 
     ),
-    #className='card-deck py-4',
-    style= {"margin-top": "20px"},
-     
-),
 
-dbc.Card(
-    dbc.CardBody(
-        [
-            #dbc.CardFooter("Trip Mode Choice"),
-            #dcc.Graph(id='mode-choice-graph'),
-            #html.Br(),
-            html.H2('Trip Departure Hour:'),
-            dcc.Graph(id='trip-deptm-graph'),
+    dbc.Card(
+        dbc.CardBody(
+            [
+                #dbc.CardFooter("Trip Mode Choice"),
+                #dcc.Graph(id='mode-choice-graph'),
+                #html.Br(),
+                html.H2('Trip Departure Hour:'),
+                dcc.Graph(id='trip-deptm-graph'),
 
-            #html.Div(id='dummy_div'),
-        ]
-    ),
-    style= {"margin-top": "20px"},
-)
+                #html.Div(id='dummy_div'),
+            ]
+        ),
+        style={"margin-top": "20px"},
+    )
 ]
+
 
 # load drop downs
 @app.callback(
     [Output('person-type-dropdown', 'options'),
-              Output('dpurp-dropdown', 'options')],
-               [Input('trips', 'children'),
-                Input('dummy_div', 'children')])
-
+     Output('dpurp-dropdown', 'options')],
+    [Input('trips', 'children'),
+     Input('dummy_div', 'children')])
 def load_drop_downs(json_data, aux):
-    print ('trip filter callback')
+    print('trip filter callback')
     person_types = ['All']
     dpurp = ['All']
 
@@ -101,27 +102,26 @@ def load_drop_downs(json_data, aux):
 
 @app.callback([Output('mode-choice-graph', 'figure'),
                Output('trip-deptm-graph', 'figure')],
-               [Input('trips', 'children'),
-                Input('person-type-dropdown', 'value'),
-                Input('dpurp-dropdown', 'value'),
-                Input('mode-share-type', 'value')])
-
+              [Input('trips', 'children'),
+               Input('person-type-dropdown', 'value'),
+               Input('dpurp-dropdown', 'value'),
+               Input('mode-share-type', 'value')])
 def update_graph(json_data, person_type, dpurp, share_type):
-    print ('trip_update graph callback')
+    print('trip_update graph callback')
     datasets = json.loads(json_data)
     data1 = []
     data2 = []
     for key in datasets.keys():
-        print (key)
+        print(key)
         df = pd.read_json(datasets[key], orient='split')
         if person_type != 'All':
-            df =df[df['pptyp'] == person_type] 
+            df = df[df['pptyp'] == person_type]
         if dpurp != 'All':
-            df =df[df['dpurp'] == dpurp]
+            df = df[df['dpurp'] == dpurp]
         if share_type == 'Mode Share':
-            df_mode_share= df[['mode','trexpfac']].groupby('mode').sum()[['trexpfac']]/df[['trexpfac']].sum() * 100
+            df_mode_share = df[['mode', 'trexpfac']].groupby('mode').sum()[['trexpfac']]/df[['trexpfac']].sum() * 100
         else:
-            df_mode_share= df[['mode','trexpfac']].groupby('mode').sum()[['trexpfac']]
+            df_mode_share = df[['mode', 'trexpfac']].groupby('mode').sum()[['trexpfac']]
         df_mode_share.reset_index(inplace=True)
 
         # mode choice graph
@@ -133,37 +133,34 @@ def update_graph(json_data, person_type, dpurp, share_type):
         data1.append(trace1)
 
         # trip distance histogram
-        df_deptm_share= df[['deptm_hr','trexpfac']].groupby('deptm_hr').sum()[['trexpfac']]/df[['trexpfac']].sum() * 100
+        df_deptm_share = df[['deptm_hr', 'trexpfac']].groupby('deptm_hr').sum()[['trexpfac']]/df[['trexpfac']].sum() * 100
         #print (df_deptm_share)
-        #df_deptm_share= df[['deptm_hr','trexpfac']].groupby('deptm_hr').sum()[['trexpfac']]
+        #df_deptm_share= df[['deptm_hr', 'trexpfac']].groupby('deptm_hr').sum()[['trexpfac']]
         df_deptm_share.reset_index(inplace=True)
-       
+
         trace2 = go.Bar(
             x=df_deptm_share['deptm_hr'],
             y=df_deptm_share['trexpfac'].astype(int),
-            name= key
-)
+            name=key
+        )
         data2.append(trace2)
         #data.append[trace]
 
     layout1 = go.Layout(
-            barmode = 'group',
+            barmode='group',
             xaxis={'title': 'mode'},
-            yaxis={'title': share_type, 'zeroline':False},
+            yaxis={'title': share_type, 'zeroline': False},
             hovermode='closest',
             autosize=True,
             font=dict(family='Segoe UI', color='#7f7f7f')
             )
 
     layout2 = go.Layout(
-            barmode = 'group',
+            barmode='group',
             xaxis={'title': 'departure hour'},
-            yaxis={'title': 'share', 'zeroline':False},
+            yaxis={'title': 'share', 'zeroline': False},
             hovermode='closest',
             autosize=True,
             font=dict(family='Segoe UI', color='#7f7f7f')
             )
     return {'data': data1, 'layout': layout1}, {'data': data2, 'layout': layout2}
-
-    
-    
