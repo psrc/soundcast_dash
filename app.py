37bf64feb06080a2ed18cb237ecdd54863ac6554
DianaM <37599121+DianaM01@users.noreply.github.com>
2020-07-12 12:09:11 -0700

    update Tour Mode Choice tab

--- app.py
+++ app.py
@@ -210,6 +210,24 @@ tab_tours_mc_filter = [dbc.Card(
                     id='tour-dpurp-dropdown'
                 ),
                 html.Br(),
+                dbc.Label('Trip End:'),
+                dbc.RadioItems(
+                    options=[{'label': i, 'value': i} for i
+                                 in ['Origin', 'Destination']],
+                    value='Origin',
+                    id='tour-trip-end'
+                    
+                ),
+                html.Br(),
+                dbc.Label('District:'),
+                dcc.Dropdown(
+                    value='All',
+                    clearable=False,
+                    id='tour-trip-district'
+
+                ),
+                
+                html.Br(),
                 #html.Div(id='df', style={'display': 'none'}),
                 html.Div(id='dummy_div2'),
             ],
@@ -942,61 +960,86 @@ def update_graph(scenario1, scenario2, scenario3, person_type, dpurp, end, end_d
 # load drop downs
 @app.callback(
     [Output('tour-person-type-dropdown', 'options'),
-     Output('tour-dpurp-dropdown', 'options')],
+     Output('tour-dpurp-dropdown', 'options'),
+     Output('tour-trip-district', 'options')],
     [Input('dummy_div2', 'children')])
 def tour_load_drop_downs(aux):
 
-    return [{'label': i, 'value': i} for i in config['person_type_list']], [{'label': i, 'value': i} for i in config['trip_purpose_list']]
+    return [{'label': i, 'value': i} for i in config['person_type_list']], [{'label': i, 'value': i} for i in config['trip_purpose_list']], [{'label': i, 'value': i} for i in config['district_list']]
+ 
  
 @app.callback([Output('tour-mode-choice-graph', 'figure'),
                Output('tour-deptm-graph', 'figure')],
-              [Input('tours', 'children'),
+              [Input('scenario-1-dropdown', 'value'),
+               Input('scenario-2-dropdown', 'value'),
+               Input('scenario-3-dropdown', 'value'),
+              #Input('tours', 'children'),
                Input('tour-person-type-dropdown', 'value'),
                Input('tour-dpurp-dropdown', 'value'),
+               Input('tour-trip-end', 'value'),
+               Input('tour-trip-district', 'value'),
                Input('tour-mode-share-type', 'value'),
                Input('tour-mode-share-type-deptm', 'value')])
-def tour_update_graph(json_data, person_type, dpurp, share_type, share_type_deptm):
+def tour_update_graph(scenario1, scenario2, scenario3, person_type, dpurp, end, end_district, 
+                      share_type, share_type_deptm):
 
-    datasets = json.loads(json_data)
+    #datasets = json.loads(json_data)
     data1 = []
     data2 = []
-    for key in datasets.keys():
-        df = pd.read_json(datasets[key], orient='split')
-        if person_type != 'All':
-            df = df[df['pptyp'] == person_type]
-        if dpurp != 'All':
-            df = df[df['pdpurp'] == dpurp]
-        if share_type in ['Distribution','Mode Share']:
-            df_mode_share = df[['tmodetp', 'toexpfac']].groupby('tmodetp')\
-                .sum()[['toexpfac']]/df[['toexpfac']].sum() * 100
-        else:
-            df_mode_share = df[['tmodetp', 'toexpfac']].groupby('tmodetp')\
-                .sum()[['toexpfac']]
-        df_mode_share.reset_index(inplace=True)
+    scenario_list = [scenario1, scenario2, scenario3]
+    
+    trips_dict = {}
+    tours_dict = {}
+    persons_dict = {}
+    
+    #for key in datasets.keys():    
+        #df = pd.read_json(datasets[key], orient='split')
+        
+    for x in range(0, len(scenario_list)):
+        if scenario_list[x] is not None:
+            if end_district != 'All':
+                end_type = 'o' if end == 'Origin' else 'd'
+                #print(os.path.join('data', scenario_list[x], 'tour_tlvorig_tour_' + end_type + '_district_' + end_district + '.csv'))
+                df = pd.read_csv(os.path.join('data', scenario_list[x], 'tour_tlvorig_tour_' + end_type + '_district_' + end_district + '.csv'))  
+            else:
+                df = pd.read_csv(os.path.join('data', scenario_list[x], 'tour_tlvorig.csv'))   
+            if person_type != 'All':
+                df = df[df['pptyp'] == person_type]
+            if dpurp != 'All':
+                df = df[df['pdpurp'] == dpurp]
+            if share_type in ['Distribution','Mode Share']:
+                df_mode_share = df[['tmodetp', 'toexpfac']].groupby('tmodetp')\
+                    .sum()[['toexpfac']]/df[['toexpfac']].sum() * 100
+            else:
+                df_mode_share = df[['tmodetp', 'toexpfac']].groupby('tmodetp')\
+                    .sum()[['toexpfac']]
+            df_mode_share.reset_index(inplace=True)
 
-        # mode choice graph
-        trace1 = go.Bar(
-            x=df_mode_share['tmodetp'].copy(),
-            y=df_mode_share['toexpfac'].copy(),
-            name=key
+            # mode choice graph
+            trace1 = go.Bar(
+                x=df_mode_share['tmodetp'].copy(),
+                y=df_mode_share['toexpfac'].copy(),
+                #name=key
+                name=scenario_list[x]
+                )
+            data1.append(trace1)
+
+             # trip distance histogram
+            if share_type_deptm == 'Distribution':
+                df_deptm_share = df[['tlvorg_hr', 'toexpfac']].groupby('tlvorg_hr')\
+                    .sum()[['toexpfac']]/df[['toexpfac']].sum() * 100
+            else:
+                df_deptm_share = df[['tlvorg_hr', 'toexpfac']].groupby('tlvorg_hr')\
+                    .sum()[['toexpfac']]
+            df_deptm_share.reset_index(inplace=True)
+
+            trace2 = go.Scatter(
+                x=df_deptm_share['tlvorg_hr'],
+                y=df_deptm_share['toexpfac'].astype(int),
+                #name=key
+                name=scenario_list[x]
             )
-        data1.append(trace1)
-
-         # trip distance histogram
-        if share_type_deptm == 'Distribution':
-            df_deptm_share = df[['tlvorg_hr', 'toexpfac']].groupby('tlvorg_hr')\
-                .sum()[['toexpfac']]/df[['toexpfac']].sum() * 100
-        else:
-            df_deptm_share = df[['tlvorg_hr', 'toexpfac']].groupby('tlvorg_hr')\
-                .sum()[['toexpfac']]
-        df_deptm_share.reset_index(inplace=True)
-
-        trace2 = go.Scatter(
-            x=df_deptm_share['tlvorg_hr'],
-            y=df_deptm_share['toexpfac'].astype(int),
-            name=key
-        )
-        data2.append(trace2)
+            data2.append(trace2)
         
     layout1 = go.Layout(
             barmode='group',
