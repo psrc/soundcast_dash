57d4d15ad61d12c85e7f81661b845e147be70391
Brice Nichols <BNichols@psrc.org>
2020-08-17 16:36:09 -0700

    agency filter removed for scenario transit boardings

--- app.py
+++ app.py
@@ -872,62 +872,6 @@ tab_transit_boardings_layout = [
             ]
         ), style={"margin-top": "20px"}
     ),
-    # dbc.Card(
-    # dbc.CardBody(
-    #     [
-    #         html.H2(id='traffic-totals-header'),
-    #         html.Br(),
-    #         html.Div(id='traffic-totals-container'),
-    #         ]
-    #     ), style={"margin-top": "20px"}
-    # ),
-
-    # dbc.Row(children=[
-    #      dbc.Col(
-    #         dbc.Card(
-    #             dbc.CardBody(
-    #                 [
-    #         html.H2(id='boardings-header'),
-    #         html.Br(),
-    #         html.Div(id='boardings-container'),
-    #                     ]
-    #                 ), style={"margin-top": "20px"}
-    #             ),
-    #         width=12
-    #         ),  # end Col
-    #     ]
-        # ),  # end Row
-    # dbc.Row(children=[
-    #      dbc.Col(
-    #         dbc.Card(
-    #             dbc.CardBody(
-    #                 [
-    #         html.H2(id='key-route-header'),
-    #         html.Br(),
-    #         html.Div(id='key-route-boardings'),
-    #                     ]
-    #                 ), style={"margin-top": "20px"}
-    #             ),
-    #         width=12
-    #         ),  # end Col
-    #     ]
-    #     ),  # end Row
-    # dbc.Row(children=[
-    #      dbc.Col(
-    #         dbc.Card(
-    #             dbc.CardBody(
-    #                 [
-    #         html.H2(id='light-rail-boardings-header'),
-    #         html.Br(),
-    #         html.Div(id='light-rail-boardings'),
-    #                     ]
-    #                 ), style={"margin-top": "20px"}
-    #             ),
-    #         width=12
-    #         ),  # end Col
-    #     ]
-    #     ),  # end Row
-
 ]
 
 tab_transit_boardings_filter = [dbc.Card(
@@ -959,8 +903,6 @@ tab_transit_boardings_filter = [dbc.Card(
                 ],
                 ), 
             style={"margin-top": "20px"}
-            #className = 'bg-light',
-            
             ),
             html.Div(id='dummy_div10'),
         ],
@@ -979,8 +921,6 @@ tab_transit_boardings_scenario_layout = [
             html.H2(id='boardings-header-scenario'),
             html.Br(),
             html.Div(id='boardings-container-scenario'),
-            html.Br(),
-            html.Div(id='scen-list'),
                         ]
                     ), style={"margin-top": "20px"}
                 ),
@@ -1023,24 +963,23 @@ tab_transit_boardings_scenario_layout = [
 
 tab_transit_boardings_scenario_filter = [dbc.Card(
     [
-        dbc.CardHeader(html.H1('Filters')),
+        # dbc.CardHeader(),
         dbc.Card(
-            dbc.CardBody(
-                [
-                    dbc.Label('Agency:'),
-                    dcc.Dropdown(
-                        value='All',
-                        clearable=False,
-                        id='agency-selection-scenario'
-                    ),
-                    html.Br(),
-                    html.Div(id='dummy_div11'),
-                ],
-                ), 
-            style={"margin-top": "20px"}
-            #className = 'bg-light',
-            
+            # dbc.CardBody(
+                # [
+                #     dbc.Label('Agency:'),
+                #     dcc.Dropdown(
+                #         value='All',
+                #         clearable=False,
+                #         id='agency-selection-scenario'
+                #     ),
+                #     html.Br(),
+                # ],
+                # ), 
+            # style={"margin-top": "20px"}
             ),
+            html.Div(id='dummy_div11'),
+            html.Div(id='scen-list'),
         ],
         className='aside-card',
         
@@ -2578,12 +2517,12 @@ def update_visuals(agency, selected_scen, aux):
 #####################################################
 # Transit boardings comparisons across scenarios tab
 @app.callback(
-    [Output('agency-selection-scenario','options'),
-     Output('scen-list','value')],
+    [Output('scen-list','value')],
     [Input('scenario-1-dropdown', 'value'),
      Input('scenario-2-dropdown', 'value'),
-     Input('scenario-3-dropdown', 'value')])
-def transit_load_drop_downs(scen1, scen2, scen3):
+     Input('scenario-3-dropdown', 'value'),
+     Input('dummy_div11', 'children')])
+def transit_load_drop_downs(scen1, scen2, scen3, aux):
     scen_list = []
     for scen in [scen1, scen2, scen3]:
         # Validation data only available for scenario runs, check if it exists before adding to available scen list
@@ -2591,30 +2530,27 @@ def transit_load_drop_downs(scen1, scen2, scen3):
         if os.path.isfile(fname_path):
             scen_list.append(scen)
 
-    return [[{'label': i, 'value': i} for i in config['agency_list']], scen_list]
+    return [scen_list]
 
 @app.callback(
     [Output('boardings-container-scenario', 'children'),
      Output('key-route-boardings-scenario', 'children'),
      Output('light-rail-boardings-scenario', 'children')],
      [Input('scen-list','value'),
-     Input('agency-selection-scenario', 'value'),
      Input('dummy_div11', 'children')]
     )
 # def update_visuals(scen1, scen2, scen3, agency, aux):
-def update_visuals(scen_list, agency, aux):
-
+def update_visuals(scen_list, aux):
     def compile_csv_to_dict(filename, scenario_list):
         dfs = list(map(lambda x: pd.read_csv(os.path.join('data', x, filename)), scenario_list))
         dfs_dict = dict(zip(scenario_list, dfs))
         return(dfs_dict)
 
-    def process_dataset(dataset, agency, scen_list, index_col, join_col):
+    def process_dataset(dataset, scen_list, index_col, join_col):
         result = []
+        # print(agency)
         for scen in scen_list:
             df = pd.DataFrame(dataset[scen])
-            if agency != 'All':
-                df = df[df['agency'] == agency] 
             df.set_index(index_col, inplace=True)
             df.rename(columns={'modeled_5to20': scen}, inplace=True)
             df.loc['Total'] = df.sum(numeric_only=True, axis=0)
@@ -2630,7 +2566,7 @@ def update_visuals(scen_list, agency, aux):
 
         return result
 
-    def create_totals_table(id_name, table):
+    def create_totals_table(table):
         t = html.Div(
             [
                 dash_table.DataTable(
@@ -2645,16 +2581,16 @@ def update_visuals(scen_list, agency, aux):
         return t
 
     dataset = compile_csv_to_dict('daily_boardings_by_agency.csv', scen_list)
-    result = process_dataset(dataset, agency, scen_list, index_col='agency', join_col='agency')
-    agency_boardings = create_totals_table('boardings-container-scenario', result)
+    result = process_dataset(dataset, scen_list, index_col='agency', join_col='agency')
+    agency_boardings = create_totals_table(result)
 
     dataset = compile_csv_to_dict('daily_boardings_key_routes.csv', scen_list)
-    result = process_dataset(dataset, agency, scen_list, index_col='description', join_col=['description','agency'])
-    key_route_boardings = create_totals_table('key-route-boardings-scenario-scenario', result)
+    result = process_dataset(dataset, scen_list, index_col='description', join_col=['description','agency'])
+    key_route_boardings = create_totals_table(result)
 
     dataset = compile_csv_to_dict('light_rail_boardings.csv', scen_list)
-    result = process_dataset(dataset, agency, scen_list, index_col='station_name', join_col='station_name')
-    light_rail_boardings = create_totals_table('light-rail-boardings-scenario', result)
+    result = process_dataset(dataset, scen_list, index_col='station_name', join_col='station_name')
+    light_rail_boardings = create_totals_table(result)
 
     return agency_boardings, key_route_boardings, light_rail_boardings
 
